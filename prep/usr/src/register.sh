1ab675f7205cb64bafc2a256f1951928642a3e35
Michael L <mike@michaellawrenceit.com>
2018-09-27 09:27:52 -0500

    Update register.sh

--- prep/usr/src/register.sh
+++ prep/usr/src/register.sh
@@ -15,6 +15,7 @@ touch /proc/sys/fs/binfmt_misc/register
 [ -d /proc/sys/fs/binfmt_misc ] || modprobe binfmt_misc
 [ -f /proc/sys/fs/binfmt_misc/register ] || mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc 
 #
+# https://wiki.gentoo.org/wiki/Embedded_Handbook/General/Compiling_with_qemu_user_chroot 
 echo ':aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/usr/bin/qemu-aarch64:' > /proc/sys/fs/binfmt_misc/register
 echo ':arm:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/qemu-wrapper:' > /proc/sys/fs/binfmt_misc/register
 echo ':armeb:M::\x7fELF\x01\x02\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x28:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/usr/bin/qemu-armeb:' > /proc/sys/fs/binfmt_misc/register
@@ -24,7 +25,7 @@ echo ':mipsel:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00
 echo ':ppc:M::\x7fELF\x01\x02\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x14:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/usr/bin/qemu-ppc:' > /proc/sys/fs/binfmt_misc/register
 echo ':sh4:M::\x7fELF\x01\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x2a\x00:\xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfb\xff\xff\xff:/usr/bin/qemu-sh4:' >/proc/sys/fs/binfmt_misc/register
 echo ':sh4eb:M::\x7fELF\x01\x02\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x2a:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/usr/bin/qemu-sh4eb:' >/proc/sys/fs/binfmt_misc/register
-echo ':sparc:M::\x7fELF\x01\x02\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x02:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/usr/bin/qemu-sparc:' > /proc/sys/fs/binfmt_misc/register 
+echo ':sparc:M::\x7fELF\x01\x02\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x02:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/usr/bin/qemu-sparc:' > /proc/sys/fs/binfmt_misc/register
 #
 #OpenRC
 /etc/init.d/qemu-binfmt start
@@ -35,7 +36,18 @@ systemctl status systemd-binfmt
 #### Install the static qemu into the chroot: 
 ## failsafe way
 ### emerge app-emulation/qemu-arm-wrappers on host /Chroot preferable 
-cat <<EOF > /qemu-wrapper.c
+# https://github.com/sakaki-/gentoo-on-rpi3-64bit/wiki/Build-aarch64-Packages-on-your-PC%2C-with-User-Mode-QEMU-and-binfmt_misc
+cat <<EOF > /qemu-aarch64-wrapper.c
+/*
+ * Call QEMU binary with additional "-cpu cortex-a53" argument.
+ *
+ * Copyright (c) 2018 sakaki <sakaki@deciban.com>
+ * License: GPL v3.0+
+ *
+ * Based on code from the Gentoo Embedded Handbook
+ * ("General/Compiling_with_qemu_user_chroot")
+ */
+
 #include <string.h>
 #include <unistd.h>
 
@@ -44,12 +56,12 @@ int main(int argc, char **argv, char **envp) {
 
 	newargv[0] = argv[0];
 	newargv[1] = "-cpu";
-	newargv[2] = "cortex-a8";
+	newargv[2] = "cortex-a53";
 
-	memcpy(&newargv[3], &argv[1], sizeof(*argv) * (argc - 1));
+	memcpy(&newargv[3], &argv[1], sizeof(*argv) * (argc -1));
 	newargv[argc + 2] = NULL;
-	return execve("/usr/bin/qemu-arm", newargv, envp);
+	return execve("/usr/local/bin/qemu-aarch64", newargv, envp);
+}
 EOF
 ### compile that.
-gcc -static qemu-wrapper.c -O3 -s -o qemu-wrapper
-mv qemu-wrapper /usr/bin/
+gcc -static qemu-aarch64-wrapper.c -O3 -s -o qemu-aarch64-wrapper
